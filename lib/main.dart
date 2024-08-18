1bf24d4ce54e19de5b5fb88d8b58bda71c4de6fe
may <soo.toance@gmail.com>
2024-08-18 18:32:02 +0900

    image 위젯 이용해서 이미지 출력

--- lib/main.dart
+++ lib/main.dart
@@ -14,8 +14,10 @@ class SplashScreen extends StatelessWidget {
             color: Colors.orange,
           ),
         child: Center(
-          child: Text('Splash Screen'),
-        ),
+          child: Image.asset(
+            'assets/logo.png'
+          ),
+        )
       ),
     ),
   );
